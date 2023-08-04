a7c8536b6b2defaf469489ea95643587a3e34efb
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-04 16:23:31 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -24,3 +24,4 @@ class Notepad:
         file_menu.add_command(label="Save", command=self.save_file, accelerator="Ctrl+S")
         file_menu.add_command(label="Save As", command=self.save_file_as, accelerator="Ctrl+Shift+S")
         file_menu.add_separator()
+        file_menu.add_command(label="Exit", command=self.root.quit, accelerator="Alt+F4")
