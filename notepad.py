fdee5d382271f58aa4a68021a1669a8cfa011a3a
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-04 16:26:50 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -35,3 +35,4 @@ class Notepad:
         view_menu = tk.Menu(menubar, tearoff=0)
         view_menu.add_command(label="Toggle Dark Mode", command=self.toggle_dark_mode, accelerator="Ctrl+T")
         menubar.add_cascade(label="View", menu=view_menu)
+
