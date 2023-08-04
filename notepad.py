02aa1135627cf228d8e06079327acddb843ced33
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-04 16:29:24 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -46,3 +46,4 @@ class Notepad:
         self.root.bind_all("<Control-z>", lambda event: self.text_area.edit_undo())
         self.root.bind_all("<Control-y>", lambda event: self.text_area.edit_redo())
         self.root.bind_all("<Any-KeyPress>", self.update_status_bar)
+        self.root.bind_all("<Control-T>", lambda event: self.toggle_dark_mode())
