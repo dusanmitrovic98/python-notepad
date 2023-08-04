385fec8b44ad1d25b57c78197b437ee0ffdfc4a4
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-04 16:17:44 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -8,3 +8,4 @@ class Notepad:
         self.text_area = tk.Text(self.root, undo=True)
         self.text_area.pack(expand=True, fill='both')
         self.current_file = None
+        self.dark_mode = False  # Default is light mode
