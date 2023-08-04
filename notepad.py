422674175c6d5b2903a1c77ee0a82de77d8f8624
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-04 16:17:23 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -6,3 +6,4 @@ class Notepad:
         self.root = root
         self.root.title("Notepad")
         self.text_area = tk.Text(self.root, undo=True)
+        self.text_area.pack(expand=True, fill='both')
