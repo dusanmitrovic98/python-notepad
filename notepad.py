d0f82881d60655d2e5c1afbe7a23ba7f85292916
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-08-05 16:47:07 +0200

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -90,3 +90,4 @@ class Notepad:
     def toggle_dark_mode(self):
         self.dark_mode = not self.dark_mode
         bg_color = "#505050" if self.dark_mode else "white"
+        fg_color = "white" if self.dark_mode else "black"
